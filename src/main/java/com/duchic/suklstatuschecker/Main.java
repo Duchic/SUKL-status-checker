b057abb05aa2ea896e5892631c141923ca97c791
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2023-08-01 21:57:54 +0200

    Main class, set fetch target website

--- src/main/java/com/duchic/suklstatuschecker/Main.java
+++ src/main/java/com/duchic/suklstatuschecker/Main.java
@@ -1,7 +1,23 @@
 package com.duchic.suklstatuschecker;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Main {
+
+    Document doc;
+
     public static void main(String[] args) {
 
+    }
+
+    public void fetchTargetWebsite(String url) {
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
